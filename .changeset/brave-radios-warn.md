569c1610a25857090714d4cf20290354f47d086b
sunpcm <sunpcm@163.com>
2025-12-31 17:05:47 +0800

    feat: Refactor code structure for improved readability and maintainability

---
+++ .changeset/brave-radios-warn.md
@@ -0,0 +1,5 @@
+---
+"@niu/ui-lib": patch
+---
+
+ui changed
--- packages/ui-lib/index.tsx
+++ packages/ui-lib/index.tsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import React from "react";
 
 export const Button = ({ children }: { children: React.ReactNode }) => {
   return (
-    <button style={{ padding: '10px 20px', background: 'blue', color: 'white' }}>
+    <button
+      style={{ padding: "10px 18px", background: "blue", color: "white" }}
+    >
       Shared: {children}
     </button>
   );
